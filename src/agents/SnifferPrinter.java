e80604bd2e21421e71b11e4c3337d0d339b8ef53
André Cruz <andrecruz97@gmail.com>
2018-10-18 17:15:18 +0100

    sniffer agent to print to file

---
+++ src/agents/SnifferPrinter.java
@@ -0,0 +1,18 @@
+package agents;
+
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.tools.sniffer.Sniffer;
+import jade.util.leap.List;
+
+public class SnifferPrinter extends Sniffer {
+
+    @Override
+    public ACLMessage getSniffMsg(List agents, boolean onFlag) {
+        ACLMessage msg = super.getSniffMsg(agents, onFlag);
+
+        System.out.println("\nMessage Sniffed:");
+        System.out.println(msg);
+        return msg;
+    }
+}
