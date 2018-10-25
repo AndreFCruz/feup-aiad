736d420a8a03e5921487965767d3a48668c2c39d
EdgarACarneiro <up201503784@fe.up.pt>
2018-10-25 17:39:43 +0100

    simple sajas test agent

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,10 @@
-import jade.core.Profile;
-import jade.core.ProfileImpl;
 import jade.wrapper.StaleProxyException;
-import launchers.BrokerLauncher;
-import launchers.GuiLauncher;
-import launchers.Launcher;
-import jade.core.Agent;
-import jade.core.Runtime;
-import jade.wrapper.AgentController;
-import jade.wrapper.ContainerController;
-import launchers.ProducerLauncher;
 
 public class Main {
 
     public static void main(String[] args) throws StaleProxyException {
 
-        System.out.println("Starting GUI");
-        GuiLauncher l = new GuiLauncher();
-        l.launch();
 
-        System.out.println("Starting Producers");
-        ProducerLauncher p = new ProducerLauncher(5);
-        p.launch();
-
-        System.out.println("Starting Brokers");
-        BrokerLauncher b = new BrokerLauncher(10);
-        b.launch();
 
     }
 
---
+++ src/agents/SimpleAgent.java
@@ -0,0 +1,61 @@
+package agents;
+
+import sajas.core.AID;
+import sajas.core.Agent;
+import sajas.core.behaviours.Behaviour;
+
+public class SimpleAgent extends Agent {
+
+    class SimpleBehaviour extends Behaviour {
+
+        private int count = 0;
+
+        @Override
+        public void action() {
+            System.out.println(this.getAgent().getAID() + " : " + this.count);
+        }
+
+        @Override
+        public boolean done() {
+            System.out.println(this.getAgent().getAID() + " : Bye cruel world ??");
+            return false;
+        }
+
+        @Override
+        public int onEnd() {
+            System.out.println(this.getAgent().getAID() + " : Bye cruel world...");
+            return super.onEnd();
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            System.out.println(this.getAgent().getAID() + " : Hello World");
+        }
+    }
+
+    private static int agentCount = 0;
+    private int id;
+
+    SimpleAgent() {
+        this.id = agentCount++;
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+        this.addBehaviour(new SimpleBehaviour());
+    }
+
+    @Override
+    protected void takeDown() {
+        super.takeDown();
+    }
+
+    /*
+    @Override
+    public void step() {
+        System.out.println(this.id + " Hello World!");
+    }
+    */
+}
