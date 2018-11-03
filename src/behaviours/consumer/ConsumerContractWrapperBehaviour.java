3dddc95dfc8505e1b921849974c83aa2688bb4df
Xavier Fontes <xfontes42@gmail.com>
2018-11-03 13:35:39 +0000

    commented class

--- src/behaviours/consumer/ConsumerContractWrapperBehaviour.java
+++ src/behaviours/consumer/ConsumerContractWrapperBehaviour.java
@@ -4,22 +4,25 @@ import agents.Consumer;
 import sajas.core.behaviours.Behaviour;
 import sajas.core.behaviours.WrapperBehaviour;
 
+/**
+ *  Encapsulates a behaviour from the Consumer.
+ */
 public class ConsumerContractWrapperBehaviour extends WrapperBehaviour {
 
-    public ConsumerContractWrapperBehaviour(Behaviour wrapped) {
+    ConsumerContractWrapperBehaviour(Behaviour wrapped) {
         super(wrapped);
     }
 
+    /**
+     *  This function is called when the behaviour wrapped ends.
+     * @return exit code
+     */
     public int onEnd() {
-        if (((Consumer) myAgent).hasBrokerService()) {
-            // TODO: put here next step logic
-            // AKA: listening for consumer requests (?)
-        } else {
-            // This behavior is responsible for choosing producers
+        if (!((Consumer) myAgent).hasBrokerService()) {
+            // This behavior is responsible for choosing brokers
             ConsumerContractInitiator contract = new ConsumerContractInitiator((Consumer) myAgent);
             myAgent.addBehaviour(new ConsumerContractWrapperBehaviour(contract));
         }
-
         return 0;
     }
 }
