2139e6410a4ea9cafb066ced161a5dab05bed6e7
Xavier Fontes <xfontes42@gmail.com>
2018-11-11 12:08:47 +0000

    java does not have its own Pair class, shame

--- src/behaviours/consumer/ConsumerContractInitiator.java
+++ src/behaviours/consumer/ConsumerContractInitiator.java
@@ -5,8 +5,8 @@ import agents.Consumer;
 import behaviours.FIPAContractNetInitiator;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
-import javafx.util.Pair;
 import utils.EnergyContractProposal;
+import utils.OurPair;
 
 import java.io.IOException;
 import java.util.*;
@@ -62,7 +62,7 @@ public class ConsumerContractInitiator extends FIPAContractNetInitiator {
     @Override
     protected void handleAllResponses(Vector responses, Vector acceptances) {
 
-        ArrayList<Pair<ACLMessage, EnergyContractProposal>> potentialAcceptances = new ArrayList<>();
+        ArrayList<OurPair<ACLMessage, EnergyContractProposal>> potentialAcceptances = new ArrayList<>();
 
         for (Object response : responses) {
             ACLMessage received = ((ACLMessage) response);
@@ -73,7 +73,7 @@ public class ConsumerContractInitiator extends FIPAContractNetInitiator {
                 try {
                     EnergyContractProposal ec = (EnergyContractProposal) received.getContentObject();
                     // add potential acceptances
-                    potentialAcceptances.add(new Pair<>(reply, ec));
+                    potentialAcceptances.add(new OurPair<>(reply, ec));
 
                 } catch (UnreadableException e) {
                     e.printStackTrace();
@@ -88,23 +88,23 @@ public class ConsumerContractInitiator extends FIPAContractNetInitiator {
         }
 
         // order acceptances
-        Collections.sort(potentialAcceptances, (o1, o2) -> Float.compare(o1.getValue().getMonthlyEnergyCost(), o2.getValue().getMonthlyEnergyCost()));
+        Collections.sort(potentialAcceptances, (o1, o2) -> Float.compare(o1.second.getMonthlyEnergyCost(), o2.second.getMonthlyEnergyCost()));
         float percentage = 0.2f;
         int numberOfBrokers = Math.max((int) (percentage * potentialAcceptances.size()), 1);
         Random rand = new Random();
         int indexToChoose = rand.nextInt(numberOfBrokers);
 
         for (int i = 0; i < potentialAcceptances.size(); ++i){
-            Pair<ACLMessage, EnergyContractProposal> pair = potentialAcceptances.get(i);
+            OurPair<ACLMessage, EnergyContractProposal> pair = potentialAcceptances.get(i);
             if (i == indexToChoose){
-                pair.getValue().signContract(myAgent);
+                pair.second.signContract(myAgent);
                 myConsumer.setHasBrokerService(true);
-                myConsumer.getWorldModel().addConsumerBrokerContract(pair.getValue());
-                pair.getKey().setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                myConsumer.getWorldModel().addConsumerBrokerContract(pair.second);
+                pair.first.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
             } else {
-                pair.getKey().setPerformative(ACLMessage.REJECT_PROPOSAL);
+                pair.first.setPerformative(ACLMessage.REJECT_PROPOSAL);
             }
-            acceptances.add(pair.getKey());
+            acceptances.add(pair.first);
         }
     }
 
