1cc2f265277e2d01f3c1c552806cb730df8c4a79
Xavier Fontes <xfontes42@gmail.com>
2018-11-29 16:52:55 +0000

    changed launcher to be able to run on its own

--- src/launchers/EnergyMarketLauncher.java
+++ src/launchers/EnergyMarketLauncher.java
@@ -3,6 +3,7 @@ package launchers;
 import agents.*;
 import jade.core.AID;
 import jade.core.ProfileImpl;
+import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 import recorders.ConsumerContractDataSource;
 import recorders.SatisfiedConsumersDataSource;
@@ -168,21 +169,21 @@ public class EnergyMarketLauncher extends Repast3Launcher {
     }
 
     private void displayConstructor() {
-        Object2DDisplay displayProducers = new Object2DDisplay(world);
-        displayProducers.setObjectList(producers);
-        displaySurface.addDisplayable(displayProducers, "show producers");
+//        Object2DDisplay displayProducers = new Object2DDisplay(world);
+//        displayProducers.setObjectList(producers);
+//        displaySurface.addDisplayable(displayProducers, "show producers");
+//
+//        Object2DDisplay displayBrokers = new Object2DDisplay(world);
+//        displayBrokers.setObjectList(brokers);
+//        displaySurface.addDisplayable(displayBrokers, "show brokers");
+//
+//        Object2DDisplay displayConsumers = new Object2DDisplay(world);
+//        displayConsumers.setObjectList(consumers);
+//        displaySurface.addDisplayable(displayConsumers, "show consumers");
 
-        Object2DDisplay displayBrokers = new Object2DDisplay(world);
-        displayBrokers.setObjectList(brokers);
-        displaySurface.addDisplayable(displayBrokers, "show brokers");
+//        displaySurface.display();
 
-        Object2DDisplay displayConsumers = new Object2DDisplay(world);
-        displayConsumers.setObjectList(consumers);
-        displaySurface.addDisplayable(displayConsumers, "show consumers");
-
-        displaySurface.display();
-
-        energyPlotsBuild();
+        // energyPlotsBuild();
 
     }
 
@@ -284,13 +285,13 @@ public class EnergyMarketLauncher extends Repast3Launcher {
     private void scheduleConstructor() {
 //        getSchedule().scheduleActionAtInterval(1, this, "simulationDelay", Schedule.LAST);
         getSchedule().scheduleActionAtInterval(1, this, "simulationStep");
-        getSchedule().scheduleActionAtInterval(1, energyGraphPB, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, energyGraphBC, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, energyGraphBA, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, consumersSatisfied, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, brokersEnergyWallet, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, brokersMoneyWallet, "step", Schedule.LAST);
-        getSchedule().scheduleActionAtInterval(1, this, "updateGraph", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, energyGraphPB, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, energyGraphBC, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, energyGraphBA, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, consumersSatisfied, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, brokersEnergyWallet, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, brokersMoneyWallet, "step", Schedule.LAST);
+//        getSchedule().scheduleActionAtInterval(1, this, "updateGraph", Schedule.LAST);
 
         if (STORE_RECORDS) {
             getSchedule().scheduleActionAtInterval(10, new BasicAction() {
@@ -303,7 +304,16 @@ public class EnergyMarketLauncher extends Repast3Launcher {
             getSchedule().scheduleActionAtEnd(consumersContractsRecorder, "writeToFile");
         }
 
-
+        getSchedule().scheduleActionAt(100000, new BasicAction() {
+            public void execute() {
+                try {
+                    mainContainer.getPlatformController().kill();
+                    stopSimulation();
+                } catch (ControllerException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void updateGraph() {
@@ -407,8 +417,8 @@ public class EnergyMarketLauncher extends Repast3Launcher {
 
         }
 
-        energyPlotBuildBrokers();
-        energyPlotBrokersAvailable();
+//        energyPlotBuildBrokers();
+//        energyPlotBrokersAvailable();
     }
 
     private void launchConsumers() throws StaleProxyException {
@@ -428,7 +438,7 @@ public class EnergyMarketLauncher extends Repast3Launcher {
             mainContainer.acceptNewAgent("consumer-" + i, c).start();
         }
 
-        energyPlotBuildConsumers();
+//        energyPlotBuildConsumers();
     }
 
     private Consumer makeNewConsumer(GraphicSettings gs, int agentConsumption) {
